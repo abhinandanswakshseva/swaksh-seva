2c187b4ec03332e3f5ff155d2980fac1108a9cb7
abhinandanswakshseva <abhinandanswaksh@gmail.com>
2025-02-26 06:02:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import { config } from "dotenv";
+
+import imageRoute from "./routes/imageRoutes.js";
+
+import authRoute from "./routes/authRoutes.js";
+import cors from "cors";
+import path from "path";
+config({ path: "./config/config.env" });
+
+export const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const _dirname = path.dirname("");
+const distpath = path.join(_dirname, "../Galaxy3G/dist");
+app.use(express.static(distpath));
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+// Routes
+
+app.use("/api/users",authRoute);
+app.use("/api", imageRoute)
